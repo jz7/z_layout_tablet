c9e8beadf24841fb24d30b22e6afe09aff50d593
Jan Zimpel <kontakt@janzimpel.de>
2017-02-08 23:34:04 +0100

    initial import

---
+++ languages/en/tl_page.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @package   z_layout_tablet
+ * @author    Jan Zimpel
+ * @license   GNU/LGPL
+ * @copyright Jan Zimpel 2017
+ */
+
+$GLOBALS['TL_LANG']['tl_page']['tabletLayout'] = array('Tablet page layout', 'This layout will be used if the visitor uses a Tablet.');
