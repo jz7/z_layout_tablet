4233bff0486b57539030b68bf29328e151215561
Jan Zimpel <kontakt@janzimpel.de>
2017-02-09 21:01:17 +0100

    rename repository and package

--- config/autoload.php
+++ config/autoload.php
@@ -5,7 +5,7 @@
  *
  * Copyright (c) 2005-2016 Leo Feyer
  *
- * @package   z_layout_tablet
+ * @package   z_tablet_layout
  * @author    Jan Zimpel
  * @license   GNU/LGPL
  * @copyright Jan Zimpel 2017
@@ -17,5 +17,5 @@
  */
 ClassLoader::addClasses(array
 (
-	'TabletLayout' => 'system/modules/z_layout_tablet/classes/TabletLayout.php'
+	'TabletLayout' => 'system/modules/z_tablet_layout/classes/TabletLayout.php'
 ));
--- languages/en/tl_page.php
+++ languages/en/tl_page.php
@@ -5,7 +5,7 @@
  *
  * Copyright (c) 2005-2016 Leo Feyer
  *
- * @package   z_layout_tablet
+ * @package   z_tablet_layout
  * @author    Jan Zimpel
  * @license   GNU/LGPL
  * @copyright Jan Zimpel 2017
